1f13f494c4224bc7c00f32aa0a86760237920958
seokimun <gimun08@naver.com>
2024-11-08 20:51:35 +0900

    feat: 🔧 middleware 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './common/logger/logger.middleware';
 
 @Module({
     imports: [CatsModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
