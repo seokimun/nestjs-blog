38ee69f37f14017e4a652e8d4a945f5b54b0f4a5
seokimun <gimun08@naver.com>
2024-11-09 20:04:39 +0900

    feat: 🔧 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,6 +13,19 @@ async function bootstrap() {
 
     app.useGlobalFilters(new HttpExceptionFilter());
     app.useGlobalPipes(new ValidationPipe());
+
+    app.enableCors({
+        origin: true, //어떤 프론트사이트든 접근허용 //개발이 완료되면 false로 바꿔줄것
+        credentials: true,
+    });
+
+    const config = new DocumentBuilder()
+        .setTitle('blog API')
+        .setDescription('API문서 입니다.')
+        .setVersion('1.0.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
 
     await app.listen(port);
     console.log(`${port}번 포트 실행 중....`);
