a0a52c3f527ca2ec03a570933819f2504b50d906
seokimun <gimun08@naver.com>
2024-11-10 20:07:54 +0900

    fix: 🚑 롤백

--- src/entities/cat.entity.ts
+++ src/entities/cat.entity.ts
@@ -2,35 +2,19 @@ import { Column, Entity } from 'typeorm';
 import { BaseEntity } from './BaseEntity';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { Exclude } from 'class-transformer';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('cats')
 export class Cats extends BaseEntity {
-    @ApiProperty({
-        example: 'abc@naver.com',
-        description: 'email',
-        required: true,
-    })
     @IsEmail()
     @IsNotEmpty()
     @Column({ unique: true, nullable: false })
     email: string;
 
-    @ApiProperty({
-        example: 'seokimun',
-        description: 'nickname',
-        required: true,
-    })
     @IsString()
     @IsNotEmpty()
     @Column({ nullable: false })
     nickname: string;
 
-    @ApiProperty({
-        example: '1234',
-        description: 'password',
-        required: true,
-    })
     @IsString()
     @IsNotEmpty()
     @Exclude()
